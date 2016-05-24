cd99c18708549188974891eb7e6ed4eae7e3ba93
Aaron Porter and Lucy Berger <lucyblueglass@gmail.com>
2016-05-23 20:14:04 -0700

    add js for form

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#blanks form").submit(function(event){
+    var person1Input =$("input#person1").val();
+    var person2Input =$("input#person2").val();
+    var animalInput =$("input#animal").val();
+    var exclamationInput =$("input#exclamation").val();
+    var verbInput =$("input#verb").val();
+    var nounInput =$("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $("#story").show();
+    event.preventDefault();
+  });
+});
